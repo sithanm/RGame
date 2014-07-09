66aed0b5998150b5b09664873168e1ddc0a3b726
sithanm <mathis.schaefer@gmx.de>
2014-07-09 17:51:29 +0200

    changed canIpass System

--- map/Block.py
+++ map/Block.py
@@ -1,6 +1,6 @@
 class Block(object):
 	def __init__(self, typ, texture):
-		self.typ_block=typ #0=durchgehen, 1=nicht durchgehen
+		self.typ_block=typ #alles geraden(+0) durchgehen, alle ungerade nicht durchgehen
 		self.texture_block=texture
 		self.x_pos_block
 		self.y_pos_block
@@ -10,7 +10,10 @@ class Block(object):
 		return self.x_pos_block, self.y_pos_block
 
 	def canIpass(self):
-		return self.typ_block   #!!!! nachbessern, sehr unsauber, da typ_block noch andere Werte in Zukunft bekommen wird!!!!
+		if self.typ_block%2 == 0:
+			return 0
+		else:
+			return 1
 
 
 	def setTyp(self, typ):
